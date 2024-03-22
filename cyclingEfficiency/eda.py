24dea61a8819136880ceda8ebf120e433cdaf0fd
John Ortiz <jaortiz92@hotmail.com>
2024-03-21 23:10:11 -0500

    Completed class EDA

--- cyclingEfficiency/eda.py
+++ cyclingEfficiency/eda.py
@@ -7,34 +7,49 @@ from .utils import Constants, Utils
 
 class Eda:
     def __init__(
-            self, data: DataFrame, bike_weight: float
+            self, data: DataFrame, bike_weight: float,
+            cad_min: int, cad_max: int, cad_step: int
         ) -> None:
         """
         This class add and transform information
 
         Parameters:
+        -----------
 
         data (DataFrame):
             DataFrame with information
+        bike_weight (float):
+            Total mass, it must have the weight
+            of a cyclist and his bicycle.
+        cad_min (int):
+            Value lower to use in the cadence
+        cad_max (int):
+            Value upper to use in the cadence
+        cad_step (int):
+            Step to range in cadence zone
         """
         self.data: DataFrame = data
         self.bike_weight: float = bike_weight
-        self.add_variable()
+        self.cad_min: int = cad_min
+        self.cad_max: int = cad_max
+        self.cad_step: int = cad_step
+        self.add_variables()
 
 
-    def add_variable(self)-> None:
+    def add_variables(self)-> None:
         """
         This method use add variables to the data
 
         Parameters:
-        
+        -----------
         None 
         
         Returns:
-        
+        --------
         None
         """
         data: DataFrame = self.data.copy()
+        # mechanical work
         data['w'] = data.apply(
             lambda x: Utils.generate_w(
                 slope=x['slope'],
@@ -47,6 +62,21 @@ class Eda:
         data['w_hr'] = data['w'] / data['hr']
         data['w_kg'] = data['w'] / data['weight']
         data['w_kg_hr'] = data['w_kg'] / data['hr']
-        data['performanca_incicator'] = data['w_kg'] / data['zones']
+
+        # Indicator
+        data['performance_indicator'] = data['w_kg'] / data['zones']
+        
+        # Cad
+        data['cad_zone'] = Utils.generate_cad_zone(
+            data['cad'], self.cad_min,
+            self.cad_max, self.cad_step
+        )
+        data = data[
+            ~data['cad_zone'].isna()
+        ].reset_index(drop=True)
+
+        data['cad_zone_num'] = data['cad_zone'].map(
+            lambda x: int(x[:2])
+        )
 
         self.data = data.copy()
--- cyclingEfficiency/utils/utils.py
+++ cyclingEfficiency/utils/utils.py
@@ -13,9 +13,28 @@ class Utils():
     @classmethod
     def generate_w(
         cls, slope: float, mass: float, 
-        velocity_km_h: float, crr: float= 0.021
+        velocity_km_h: float, crr: float= 0.02
     ) -> float:
+        """
+        This function estimates the mechanical
+        work for each instant
+
+        Parameters:
+        -----------
+        slope (float):
+            Value slope
+        mass (float):
+            Total mass, it must have the weight
+            of a cyclist and his bicycle.
+        velocity_km_h (float):
+            Value velocity in kilometers per hour
+        crr (float): = 0.02
+            Value Rolling resistance coefficient
         
+        Returns:
+        --------
+        float: mechanical work in joules
+        """        
         gravity: float = 9.8
         if slope > 0:
             angle_radians: float = np.arctan(slope/100)
@@ -25,3 +44,56 @@ class Utils():
         f_r: float = mass * gravity * crr
         d: float = velocity_km_h * 1000 / (60 * 60)
         return d * (f_r + f_g)
+    
+    @classmethod
+    def generate_cad_zone(
+            self, data: Series | list[int], cad_min: int, 
+            cad_max: int, cad_step: int
+        ) -> list[str]:
+        """
+        This function transform values in intervals
+
+        Parameters:
+        -----------
+        data (Series | list[int]):
+            Values to transform
+        cad_min (int):
+            Value lower 
+        cad_max (int):
+            Value upper
+        cad_step (int):
+            Step to range
+        
+        Returns:
+        --------
+        list[str]: List with strings and its range
+        """ 
+        cad_zones: list[int] = list(
+            range(cad_min, cad_max + 1, cad_step)
+        )
+
+        cad_zones_name: list[str] = [
+            '{}) {}-{}'.format(
+                str(i+1).zfill(2), lower, 
+                lower + cad_step - 1
+            ) 
+            for i, lower in enumerate(cad_zones)
+        ]
+
+        size: int = len(cad_zones)
+        result_cad_zones: list[int] = []
+
+        for cad in data:
+            flag: bool = True
+            index: int = size - 1
+            result: int = np.nan
+            while flag and 0 <= index:
+                if cad_zones[index] <= cad:
+                    if index == size - 1:
+                        flag = False
+                    else:
+                        result = cad_zones_name[index]
+                        flag = False
+                index -= 1  
+            result_cad_zones.append(result)
+        return result_cad_zones
